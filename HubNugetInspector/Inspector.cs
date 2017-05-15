79c47a6c0efb604c93d479eb240b3e441092b493
psantos1113 <psantos@blackducksoftware.com>
2017-05-15 09:48:52 -0400

    Adding missing files from last commit

---
+++ HubNugetInspector/Inspector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Blackducksoftware.Integration.Nuget.Inspector
+{
+    interface Inspector
+    {
+
+        string Execute();
+        void Setup();
+        DependencyNode GetNode();
+        string WriteInfoFile(DependencyNode node);
+
+    }
+}
---
+++ HubNugetInspector/InspectorUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Com.Blackducksoftware.Integration.Nuget.Inspector
+{
+    class InspectorUtil
+    {
+        public const string DEFAULT_OUTPUT_DIRECTORY = "blackduck";
+        public const string DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd_HH-mm-ss";
+
+        public static string GetProjectAssemblyVersion(string dateFormat, string projectDirectory)
+        {
+            string version = DateTime.UtcNow.ToString(dateFormat);
+            List<string> pathSegments = new List<string>();
+            pathSegments.Add(projectDirectory);
+            pathSegments.Add("Properties");
+            pathSegments.Add("AssemblyInfo.cs");
+            string path = CreatePath(pathSegments);
+
+            if (File.Exists(path))
+            {
+                List<string> contents = new List<string>(File.ReadAllLines(path));
+                var versionText = contents.FindAll(text => text.Contains("[assembly: AssemblyVersion"));
+                foreach (string text in versionText)
+                {
+                    int firstParen = text.IndexOf("(");
+                    int lastParen = text.LastIndexOf(")");
+                    // exclude the '(' and the " characters
+                    int start = firstParen + 2;
+                    // exclude the ')' and the " characters
+                    int end = lastParen - 1;
+                    version = text.Substring(start, (end - start));
+                }
+            }
+            return version;
+        }
+
+        public static string CreatePath(List<string> pathSegments)
+        {
+            return String.Join(String.Format("{0}", Path.DirectorySeparatorChar), pathSegments);
+        }
+    }
+}

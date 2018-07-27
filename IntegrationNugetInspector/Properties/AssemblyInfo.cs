852b5fefc1149940c2b357b6887c8825f0c2f611
Jordan Piscitelli <jordanp@synopsys.com>
2018-07-27 12:05:53 -0400

    Removing rc for release.

--- IntegrationNugetInspector/Properties/AssemblyInfo.cs
+++ IntegrationNugetInspector/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.5.0.0")]
 [assembly: AssemblyFileVersion("2.5.0.0")]
-[assembly: AssemblyInformationalVersion("2.5.0-rc")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
